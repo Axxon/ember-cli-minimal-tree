36150cd779ec91e9eb68a6f454cf313fa205a1b7
Sebastien Grans <sebastien.grans@gmail.com>
2015-05-29 12:11:34 +0200

    just try it

--- blueprints/ember-cli-minimal-tree/index.js
+++ blueprints/ember-cli-minimal-tree/index.js
@@ -1,15 +1,3 @@
 module.exports = {
   normalizeEntityName: function() {}
-
-
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
-
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
 };
